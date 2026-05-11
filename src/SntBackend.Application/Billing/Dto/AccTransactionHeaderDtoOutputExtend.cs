cc821882bcbb38cabc0784fb5939bce38d85a9b8
jay <nwj04290019@163.com>
2026-05-11 11:10:15 +0800

    fix bug con 分页查询添加开始和截至时间

---
+++ src/SntBackend.Application/Billing/Dto/AccTransactionHeaderDtoOutputExtend.cs
@@ -0,0 +1,10 @@
+namespace SntBackend.Application.Po.Dto
+{
+    public partial class AccTransactionHeaderDtoOutput
+    {
+        /// <summary>
+        /// 组织全名 (OrgHeader.OH_Fullname)
+        /// </summary>
+        public string ah_oh_name { get; set; }
+    }
+}

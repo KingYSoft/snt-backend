5614ab4dcca662463bc133666ceeae9df90f6adb
jay <nwj04290019@163.com>
2026-05-11 10:06:08 +0800

    fix bug ConsolidationAgentOutput

--- src/SntBackend.Application/Consolidation/Dto/ConsolidationDetailOutput.cs
+++ src/SntBackend.Application/Consolidation/Dto/ConsolidationDetailOutput.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 
 namespace SntBackend.Application.Consolidation.Dto
 {
+    public class ConsolidationAgentOutput : JobDocAddressDtoOutput
+    {
+        public OrgAddressDtoOutput org_address { get; set; }
+    }
+
     public class ConsolidationDetailOutput : JobConsolDtoOutput
     {
-        public JobDocAddressDtoOutput local_agent { get; set; }
-        public JobDocAddressDtoOutput overseas_agent { get; set; }
+        public ConsolidationAgentOutput local_agent { get; set; }
+        public ConsolidationAgentOutput overseas_agent { get; set; }
         public List<JobConsolTransportDtoOutput> transport_list { get; set; } = new();
         public List<JobShipmentDtoOutput> shps { get; set; } = new();
         public List<JobContainerDtoOutput> containers { get; set; } = new();
